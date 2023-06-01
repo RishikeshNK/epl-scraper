5808103b08c96d936524fe93784d6f63e7215f07
Rishikesh Kanabar <63854326+RishikeshNK@users.noreply.github.com>
2023-06-01 15:21:23 -0400

    Set up project skeleton

---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+
+
+def main():
+    """
+    Main function.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    start = time.time()
+    main()
+    delta = time.time() - start
+    print(f"Elapsed time: {delta:.2f}s")
